4f4108cfd8b03dcb599cd2f9b18d2159bc2c9497
ItsTiazzz <163735662+ItsTiazzz@users.noreply.github.com>
2025-12-27 01:27:20 +0100

    Make Mixin versioned
    Took 2 hours 10 minutes

--- src/main/java/org/tywrapstudios/khat/mixin/CrashReportMixin.java
+++ src/main/java/org/tywrapstudios/khat/mixin/CrashReportMixin.java
@@ -1,6 +1,7 @@
 package org.tywrapstudios.khat.mixin;
 
 import net.minecraft.CrashReport;
+//? if >=1.21
 import net.minecraft.ReportType;
 
 import org.slf4j.Logger;
@@ -13,8 +14,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.tywrapstudios.khat.logic.HandleMinecraft;
 
-import java.nio.file.Path;
-import java.util.List;
+import java.io.File;
+import java.nio.file.Path;import java.util.List;
 
 @Mixin(CrashReport.class)
 @Debug(export = true)
@@ -26,15 +27,25 @@ public abstract class CrashReportMixin {
     @Final
     private static Logger LOGGER;
 
-    @Inject(method = "saveToFile(Ljava/nio/file/Path;Lnet/minecraft/ReportType;Ljava/util/List;)Z",
+    @Inject(//? if >=1.21 {
+            method = "saveToFile(Ljava/nio/file/Path;Lnet/minecraft/ReportType;Ljava/util/List;)Z",
+            //?} else {
+            /*method = "saveToFile",
+            *///?}
             at = @At(
                     value = "INVOKE",
                     target = "Ljava/io/Writer;write(Ljava/lang/String;)V",
                     shift = At.Shift.AFTER
             )
     )
+    //? if >= 1.21 {
     private void sendWebhooksOnCrash(Path path, ReportType reportType, List<String> list, CallbackInfoReturnable<Boolean> cir) {
+    //?} else {
+    /*private void sendWebhooksOnCrash(File file, CallbackInfoReturnable<Boolean> cir) {
+    *///?}
         try {
+            //? if < 1.21
+            //Path path = file.toPath();
             HandleMinecraft.INSTANCE.handleCrash(getException(), path);
         } catch (Exception e) {
             LOGGER.error("An error occurred while trying to send the crash report to Discord.", e);
